b9f5ea4ffceb5d3b1f1c07d56233f99724f0d1ef
Brian Broeking <bbroeking456@gmail.com>
2018-05-11 19:38:19 -0400

    Initial Commit

---
+++ TickleThePig2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TickleThePig2.0
+//
+//  Created by Brian Broeking on 5/11/18.
+//  Copyright © 2018 Brian Broeking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
