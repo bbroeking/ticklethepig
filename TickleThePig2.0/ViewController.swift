de8d5081fe742c3f628b3874b3515894e83d1a26
Brian Broeking <bbroeking456@gmail.com>
2019-09-30 17:39:34 -0500

    init

--- TickleThePig2.0/ViewController.swift
+++ TickleThePig2.0/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer!
+    
+    @IBOutlet weak var TickleCounter: UILabel!
+    @IBOutlet var barnImage: UIImageView!
+
+
+    var tickleCount =  0 {
+        didSet {
+            let attributes: [NSAttributedStringKey:Any] = [
+                .strokeWidth : 2.0,
+                .strokeColor : #colorLiteral(red: 0.9271472146, green: 0.2466179687, blue: 0.9372549057, alpha: 1),
+                .font : UIFont(name: "AvenirNext-Medium", size: 40)!
+            ]
+            let attributedString = NSAttributedString(string: "Tickles: \(tickleCount)", attributes: attributes)
+            TickleCounter.attributedText = attributedString
+        }
+    }
+    
+    @IBAction func ticklePig(_ sender: Any) {
+        tickleCount += 1
+        audioPlayer.play()
+//        audioPlayer.stop()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Build the background image
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "background.png")
+        self.view.insertSubview(backgroundImage, at: 0)
+        self.barnImage.image = UIImage(named: "Cooper_Barn.png")
+        
+        // Adding Oink Sound When Tickled
+        let sound = Bundle.main.url(forResource: "SupportFiles/oink", withExtension: "mp3")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: sound!)
+            audioPlayer.prepareToPlay()
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
