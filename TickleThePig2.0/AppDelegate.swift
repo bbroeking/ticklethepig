6ab9022253b5b4aaccacfd23279cbb6f61e9da04
bbroeking <bbroeking456@gmail.com>
2019-12-17 13:39:02 -0600

    revert key

--- TickleThePig2.0/AppDelegate.swift
+++ TickleThePig2.0/AppDelegate.swift
@@ -7,8 +7,6 @@
 //
 
 import UIKit
-import Firebase
-import FirebaseFirestore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,9 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
         return true
     }
 
--- TickleThePig2.0/View Controllers/tabViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  tabViewController.swift
-//  TickleThePig2.0
-//
-//  Created by Brian Broeking on 10/7/19.
-//  Copyright © 2019 Brian Broeking. All rights reserved.
-//
-
-import Foundation
